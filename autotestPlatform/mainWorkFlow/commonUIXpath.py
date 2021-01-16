92b2186f978a7ecc1495e8d214e12a56d1fa7d9d
jane.yin <jane.yin@oocl.com>
2021-01-16 19:43:37 +0800

    Revert "init project"
    This reverts commit 710974d4ff5d36620ed16c0da044f9756f65ce34.

--- autotestPlatform/mainWorkFlow/commonUIXpath.py
+++
@@ -1,7 +0,0 @@
-
-def clickFirstLevelMenu(driver,firstMenuName):
-    driver.find_element_by_xpath("//span[text()='" + firstMenuName + "']").click()
-
-def clickSecondLevelMenu(driver,secondMenuName):
-    driver.find_element_by_xpath("//span[text()='" + secondMenuName + "']").click()
-
